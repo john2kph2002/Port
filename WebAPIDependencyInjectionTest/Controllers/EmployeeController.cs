5c0d616f131914f7c399485f728f1f92713b11bc
John Ray Cabili <jcabili@cabili.com>
2022-09-13 08:33:07 +0800

    Added Files to Repo

---
+++ WebAPIDependencyInjectionTest/Controllers/EmployeeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIDependencyInjectionTest.EmployeeData;
+using WebAPIDependencyInjectionTest.Models;
+
+namespace WebAPIDependencyInjectionTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeeController : Controller
+    {
+        private IEmployeeData _employeeData;
+        public EmployeeController(IEmployeeData employeeData)
+        {
+            _employeeData = employeeData;
+        }
+
+        [HttpGet]
+        //[Produces("application/xml")]
+        public ActionResult<IEnumerable<Employee>> Get()
+        {
+            try
+            {
+                return _employeeData.GetAllEmployees();
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpGet("{name}")]
+        public ActionResult<Employee> GetPersonByName(string name)
+        {
+            try
+            {
+                return _employeeData.GetEmployeeName(name);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<Employee> UpdatePerson(int id, Employee employee)
+        {
+            try
+            {
+                return _employeeData.UpdateEmployee(id, employee);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<Employee> Delete(int id)
+        {
+            try
+            {
+                return _employeeData.DeleteEmployee(id);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+    }
+}
---
+++ WebAPIDependencyInjectionTest/EmployeeData/EmployeeDataServices.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIDependencyInjectionTest.Models;
+
+namespace WebAPIDependencyInjectionTest.EmployeeData
+{
+    public class EmployeeDataServices : IEmployeeData
+    {
+        public readonly IEnumerable<Employee> _iEmployeeData;
+
+        public EmployeeDataServices(IEnumerable<Employee> iEmployeeData)
+        {
+            _iEmployeeData = iEmployeeData;
+        }
+
+        List<Employee> Employees = new List<Employee>
+        {
+            new Employee{Id=1, FirstName="John", MiddleName = "Ray", LastName="Doe" },
+            new Employee{Id=2, FirstName="Ray", MiddleName = "Light", LastName="Charles" },
+            new Employee{Id=3, FirstName="Sun",MiddleName = "Yet",  LastName="Lei" },
+        };
+        public List<Employee> GetAllEmployees()
+        {
+            return Employees;
+        }
+
+        public Employee GetEmployeeName(string name)
+        {
+            Employee a;
+            a = Employees.FirstOrDefault(x => x.FirstName.ToLower().ToUpper().Trim().Contains(name.ToLower().ToUpper().Trim()) || x.LastName.ToLower().ToUpper().Trim().Contains(name.ToLower().ToUpper().Trim()));
+            return a;
+        }
+
+        public Employee UpdateEmployee(int id, Employee employee)
+        {
+            Employee a = (from x in Employees where x.Id == id select x).FirstOrDefault();
+            a.FirstName = employee.FirstName;
+            a.MiddleName = employee.MiddleName;
+            a.LastName = employee.LastName;
+
+            return a;
+        }
+
+        public Employee DeleteEmployee(int id)
+        {
+            Employee a = (from x in Employees where x.Id == id select x).FirstOrDefault();
+            Employees.Remove(a);
+            return a;
+        }
+    }
+}
